0d67084ea387cd0c1fb88def354d394d9dfdc4d9
Rochelle Katzman <zelrochelle@gmail.com>
2022-11-21 10:56:08 -0500

    commit tree 563874d5e950c52269b3f60faa6f0623f5b6f04d

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue <= 400){
+    return 'This one is on me!';
+  } 
+  else if (someValue > 400 && someValue <= 2000) {
+    return 'That will be twenty bucks.';
+  }
+  else if (someValue > 2000 && someValue < 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else if (someValue >= 2500) {
+    return 'No can do.'; 
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(place){
   // Write your code here!
+  return (place === "NYC" ? "Ok, sounds good." : "No go.")
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(someVal){
   // Write your code here!
+  switch (someVal) {
+    case 'generous': 
+    return "Thank you so much."; 
+    break; 
+    case 'not as generous': 
+    return "Thank you."; 
+    break; 
+    default:
+    return "Bye."
+    break; 
+  }
 }
